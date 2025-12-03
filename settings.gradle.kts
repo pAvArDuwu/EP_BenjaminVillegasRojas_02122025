09fa8c5a73f2514dc3372bc5c75ab0850afa2f75
MiAplicacion <benjaminvillegasrojas@gmail.com>
2025-12-02 21:13:20 -0400

    Primer Commit (padre nuestro ..)

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EP_BenjaminVillegasRojas_02122025"
+include(":app")
+ 
